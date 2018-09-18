639201d0063c5eff9a283ddc42fc0149442b5f3f
MangoMango <nefilims@yahoo.com>
2018-09-18 12:19:51 +0200

    Ajout d'une dataclasse pour le Cilent et conversion des montants en property.

--- facture.py
+++ facture.py
@@ -2,7 +2,18 @@
 Module pour exercice de la formation Python avance : facturier
 """
 
-class Produit:
+from dataclasses import dataclass
+
+@dataclass
+class Client:
+    # le décorateur dataclass (Python 3.7) permet d'indiquer que la classe ne fait que contenir des données
+    # et simplifie le code en générant de manière automatique __init__ et __repr__
+    name: str
+    address: str = ''
+    phone: str = ''
+    email: str = ''
+
+class Product:
     ''' Définition d'un produit dans le catalogue. '''
 
     product_last_ref = 0
@@ -12,14 +23,16 @@ class Produit:
 
         :return: la référence créée
         '''
-        r = Produit.product_last_ref
-        Produit.product_last_ref += 1
+        r = Product.product_last_ref
+        Product.product_last_ref += 1
         return r
 
     def __init__(self, leNomProduit: str, prixUnit: float):
+        assert isinstance(leNomProduit, str) and len(leNomProduit.strip()) > 0, 'Nom du produit vide'
         self.nomProduit = leNomProduit
+        assert isinstance(prixUnit, float), 'prix non-float'
         self.prixUnitaire = prixUnit
-        self.reference = Produit.createRef()
+        self.reference = Product.createRef()
 
     def __str__(self):
         return f"{self.nomProduit}, référence: {self.reference}, prix unitaire:{self.prixUnitaire}"
@@ -28,7 +41,7 @@ class Produit:
 class LigneFacture:
     ''' Ligne sur la facture avec un seul produit mais un nombre d'unités variable '''
 
-    def __init__(self, unProduit: Produit, nbre: int):
+    def __init__(self, unProduit: Product, nbre: int):
         assert unProduit is not None , "produit vide !"
         self.produit = unProduit
         assert isinstance(nbre ,int) and nbre >= 0 , "nombre non-entier ou négatif: {0}".format(nbre)
@@ -47,13 +60,12 @@ class Facture:
     """ Instance de facture.
     """
 
-    def __init__(self, myTVA: float, myClientName: str):
+    def __init__(self, myTVA: float, myClient: Client):
         assert isinstance(myTVA,float), " TVA non-float, instance de "+str(type(myTVA))
-        self.clientName = myClientName
+        self.client = myClient
         self.TVA = myTVA
         self.refFacture = Facture.createRefFacture()
         self.lignes = []
-        self.montantTotalHT = 0.0
 
     def addLigne(self, uneLigne: LigneFacture):
         ''' Ajoute une ligne produit dans la facture
@@ -63,8 +75,13 @@ class Facture:
         '''
         assert uneLigne is not None, "ligne vide"
         self.lignes.append(uneLigne)
-        self.montantTotalHT += uneLigne.montantLigne
 
+    @property
+    def montantTotalHT(self) -> float :
+        ''' Calcule montant HT '''
+        return sum(ligne.montantLigne for ligne in self.lignes)
+
+    @property
     def montantTotalTTC(self) ->float :
         ''' Calcule le montant TTC.
 
@@ -72,6 +89,7 @@ class Facture:
         '''
         return self.montantTotalHT * (1 + self.TVA)
 
+    # numéro interne incrémental de référence
     ref_number = 1
 
     def createRefFacture() -> str :
@@ -85,6 +103,6 @@ class Facture:
         return f'INV-2018/{r}'
 
     def __str__(self):
-        return f"Facture référence={self.refFacture}, client={self.clientName}, montant HT={self.montantTotalHT} \
-               , montant TTC={self.montantTotalTTC()}, lignes:\n" + repr(self.lignes)
+        return f"Facture référence={self.refFacture}, client={self.client}, montant HT={self.montantTotalHT} \
+               , montant TTC={self.montantTotalTTC}, lignes:\n" + repr(self.lignes)
 
--- facture_unittests.py
+++ facture_unittests.py
@@ -11,6 +11,16 @@ class FactureTestCase(unittest.TestCase):
         """ appelé automatiquement en fin de test """
         pass
 
+    def test_produits(self):
+        ''' Vérifie les règles de créatiop de produits '''
+
+        # test nom de produit vide
+        with self.assertRaises(AssertionError):
+            Product(' ', 5.0)
+
+        # test nom de produit vide
+        with self.assertRaises(AssertionError):
+            Product(' ghkkkj  ', 'ceci n\'est pas un float')
 
     def testFacture(self):
         ''' Fonction de test du module
@@ -18,15 +28,15 @@ class FactureTestCase(unittest.TestCase):
         :return: rien
         '''
         print('Début testFacture ')
-        prod1 = Produit('vélo', 200.0)
-        prod2 = Produit('tricycle', 500.0)
+        prod1 = Product('vélo', 200.0)
+        prod2 = Product('tricycle', 500.0)
         assert prod1.nomProduit == 'vélo'
         assert prod1.prixUnitaire == 200.0
 
-        maFacture = Facture(myTVA=0.2, myClientName='Toto')
+        maFacture = Facture(myTVA=0.2, myClient=Client(name='Toto'))
         maFacture.addLigne(LigneFacture(prod1, 3))
         maFacture.addLigne(LigneFacture(prod2, 1))
-        assert maFacture.montantTotalTTC() == 1320.0
+        assert maFacture.montantTotalTTC == 1320.0
 
         print('Facture créée : {0}'.format(maFacture))
         print('Fin testFacture ')
@@ -38,20 +48,20 @@ class FactureTestCase(unittest.TestCase):
 
         # test d'exception pour quantité négative
         with self.assertRaises(AssertionError):
-            LigneFacture(Produit('tomate',1.0),-1)
+            LigneFacture(Product('tomate', 1.0), -1)
 
 
         # test d'exception pour quantité non-entière
         with self.assertRaises(AssertionError):
-            LigneFacture(Produit('tomate',1.0),1.5)
+            LigneFacture(Product('tomate', 1.0), 1.5)
 
         # test d'exception pour quantité non-entière
         with self.assertRaises(AssertionError):
-            Facture(myTVA='pas un numérique',myClientName='Toto')
+            Facture(myTVA='pas un numérique',myClient=Client(name='Toto'))
 
         # test d'exception avec produit None
         with self.assertRaises(AssertionError):
-            facture = Facture(myTVA=0.2, myClientName='Toto')
+            facture = Facture(myTVA=0.2, myClient=Client(name='Toto'))
             facture.addLigne(LigneFacture(None,20))
 
         print('Fin testFacture_fails ')
@@ -60,9 +70,9 @@ class FactureTestCase(unittest.TestCase):
         ''' Teste les créations sérialisées de référence. '''
         print('Début test_ref_creations ')
 
-        prod1 = Produit('produit 1',10.0)
+        prod1 = Product('produit 1', 10.0)
         print(f'Produit 1 : {prod1}')
-        nvlle_ref = Produit.createRef()
+        nvlle_ref = Product.createRef()
         print('Nouvelle référence produit 1 : {0}'.format(nvlle_ref ))
         self.assertEqual(prod1.reference,nvlle_ref-1)
 
